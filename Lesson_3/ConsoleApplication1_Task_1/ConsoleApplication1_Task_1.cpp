67986ab367a10cf3e7017563e3c211f8e7837c58
Alexander Zhuravlev <a-798@mail.ru>
2023-04-10 20:25:29 +0300

    add Lesson_3

---
+++ Lesson_3/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+	double num1, num2;
+	bool set_num1(double num1) //по условию задания в этом методе выполняем два действия: проверяем на правильность ввода и присваиваем значение полю в случае правильного ввода.
+	{
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		else
+			return false;
+	}
+	bool set_num2(double num2) // то же самое, что и в set_num1
+	{
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		else
+			return false;
+	}
+	double add ()
+	{
+		std::cout << "num1 + num2 = ";
+		return num1 + num2;
+	}
+	double multiply ()
+	{
+		std::cout << std::endl;
+		std::cout << "num1 * num2 = ";
+		return num1 * num2;
+	}
+	double subtract_1_2 ()
+	{
+		std::cout << std::endl;
+		std::cout << "num2 - num1 = ";
+		return num2 - num1;
+	}
+	double subtract_2_1 ()
+	{
+		std::cout << std::endl;
+		std::cout << "num1 - num2 = ";
+		return num1 - num2;
+	}
+	double divide_1_2 ()
+	{
+		std::cout << std::endl;
+		std::cout << "num1 / num2 = ";
+		return num1 / num2;
+	}
+	double divide_2_1 ()
+	{
+		std::cout << std::endl;
+		std::cout << "num2 / num1 = ";
+		return num2 / num1;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double num1=0, num2=0;
+	Calculator value;
+	for (;;)
+	{
+		double num1 = 0, num2 = 0; //для повторения запроса, пока сами не закроем консоль
+		while (num1 == 0)
+		{
+			std::cout << "Введите значение num1: ";
+			std::cin >> num1;
+			if (!value.set_num1(num1))
+				std::cout << "Неверный ввод!" << std::endl;
+		}
+		while (num2 == 0)
+		{
+			std::cout << "Введите значение num2: ";
+			std::cin >> num2;
+			if (!value.set_num2(num2))
+				std::cout << "Неверный ввод!" << std::endl;
+		}
+		std::cout << value.add();
+		std::cout << value.multiply();
+		std::cout << value.subtract_1_2();
+		std::cout << value.subtract_2_1();
+		std::cout << value.divide_1_2();
+		std::cout << value.divide_2_1();
+		std::cout << std::endl;
+	}
+	return 0;
+}
---
+++ Lesson_3/ConsoleApplication2_Task_2/ConsoleApplication2_Task_2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class Counter
+{
+private:
+    double value;
+public:
+    Counter()
+    {
+        this->value = 1;
+        std::cout << this->value;
+    }
+    Counter(double n)
+    {
+        this->value = n;
+    }
+    void increment()
+    {
+        this->value++;
+    }
+    void decrement()
+    {
+        this->value--;
+    }
+    void equal()
+    {
+        std::cout << this->value << std::endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    double n;
+    char ch=0;
+    Counter* value=0;
+    std::string s;
+    while (s != "да" && s != "нет")
+    {
+        std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+        getline(std::cin, s);
+       /* (s == "да") ? ((std::cout << "Введите начальное значение счётчика: ") && (std::cin >> n) && (value = new Counter(n))) :
+            ((std::cout << "Начальное значение счётчика: ") && (value = new Counter)); */ //способ через тернарный
+            if (s == "нет")
+            {
+                std::cout << "Начальное значение счётчика: ";
+                value = new Counter;
+                std::cout << std::endl;
+            }
+            else
+                if (s == "да")
+                {
+                    std::cout << "Введите начальное значение счётчика: ";
+                    std::cin >> n;
+                    value = new Counter(n);
+                }
+    }
+    while (ch != 'x')
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> ch;
+        if (ch == '+')
+            value->increment();
+        if (ch == '-')
+            value->decrement();
+        if (ch == '=')
+            value->equal();
+    } 
+    delete value;
+    std::cout << "До свидания!";
+    return 0;
+}
