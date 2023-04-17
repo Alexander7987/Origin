55af0ee6e6ba7dfc9e718266275f70aebaec4eec
Alexander Zhuravlev <a-798@mail.ru>
2023-04-17 22:40:04 +0300

    add Lesson_4

---
+++ Lesson_4/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class Adress
+{
+public:
+    std::string city, street;
+    int number_house, number_appartment;
+    void writing_in_outtxt(std::ofstream& f2)
+    {
+        f2 << city << " ";
+        f2 << street << " ";
+        f2 << number_house << " ";
+        f2 << number_appartment << std::endl;
+    } 
+}; 
+
+int main()
+{    
+    int N,i=1,j=0;
+    std::string str;
+    Adress adress;
+    std::ifstream f1("in.txt");
+    std::ofstream f2("out.txt");
+    f1 >> N;
+    f1.close(); //закроем, для безопасного начала работы циклов ниже
+    f2 << N << std::endl;
+    while (N > 0) // цикл for (j = 0; j < N; j++) не подходит, т.к. меняем значение N. Но можно N записать в др. переменную, но это увеличит кол-во переменных.
+    {
+        f1.open("in.txt");
+        while (f1 >> str) // Метод работает так: начинаем читать из файла, и, дойдя до "первой" строки последнего адреса, записываем его первым с помощью метода
+    //в файл f2. Далее уменьшаем порог (переменная N) последнего адреса на единицу и пересчитываем номер "первой" строки, но теперь уже друогого "последнего" адреса.
+        {
+            if (i == (N * 4)-3)
+            {
+                f1 >> adress.city;
+                f1 >> adress.street;
+                f1 >> adress.number_house;
+                f1 >> adress.number_appartment;
+                adress.writing_in_outtxt(f2);
+                break;
+            }
+            i++;
+        }
+        f1.close();
+        i = 1;
+        N--;
+    }
+    f1.close();
+    f2.close();
+    return 0;
+}
---
+++ Lesson_4/ConsoleApplication2_Task_2/ConsoleApplication2_Task_2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class Address
+{
+private:
+	std::string city, street;
+	int number_house, number_appartment;
+public:
+	Address(std::string city, std::string street, int number_house, int number_appartment)
+	{
+		this->city = city;
+		this->street = street;
+		this->number_house = number_house;
+		this->number_appartment = number_appartment;
+		
+	}
+	std::string str;
+	void Creating_string()
+	{
+		std::string str_1 = std::to_string(this->number_house);
+		std::string str_2 = std::to_string(this->number_appartment);
+		this->str = this->city + ", " + this->street + ", " + str_1 + ", " + str_2;
+	}
+};
+
+void Creating_outtxt(Address** address, std::ofstream& f2, int n)
+{
+	int i, j;
+	Address* w;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			if (address[j]->str[0] > address[j + 1]->str[0])
+			{
+				w = address[j + 1];
+				address[j + 1] = address[j];
+				address[j] = w; 
+			}
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		f2 << address[i]->str << std::endl;
+	} 
+}
+
+int main()
+{
+	int n,i=-1;
+	std::string city, street;
+	int number_house, number_appartment;
+	std::ifstream f1("in.txt");
+	std::ofstream f2("out.txt");
+	f1 >> n;
+	Address** address = new Address* [n];
+	while (!f1.eof())
+	{
+		i++;
+		f1 >> city;
+		f1 >> street;
+		f1 >> number_house;
+		f1 >> number_appartment;
+		address[i] = new Address(city, street, number_house, number_appartment);
+	}
+	for (i = 0; i < n; i++)
+		address[i]->Creating_string();
+	Creating_outtxt(address, f2, n);
+	f1.close();
+	f2.close();
+	return 0;
+}
