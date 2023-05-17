4165ce60677b77e3288b4329397cb15c5eec8707
Alexander Zhuravlev <a-798@mail.ru>
2023-05-17 22:34:28 +0300

    change Lesson_9

--- Lesson_9/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
+++ Lesson_9/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -5,6 +5,8 @@ class Fraction
 private:
 	int numerator_;
 	int denominator_;
+	int reserve_numerator;//Резервная переменная для числителя для действий с дробями для сокращения дроби
+	double delta = 0.1;
 
 public:
 	Fraction(int numerator, int denominator)
@@ -12,9 +14,17 @@ public:
 		numerator_ = numerator;
 		denominator_ = denominator;
 	}
+	void get_default_denominator(Fraction& other)//привожу к общему знаменателю только числители для сравнения дробей. Обязательно по ссылке для сохранения other.
+		//Внимание! other сохраняется только для текущей ВЫЗЫВАЮЩЕЙ функции. Поэтому в каждой новой перегрузке вызываем заного.
+	{
+		reserve_numerator = numerator_;
+		reserve_numerator = reserve_numerator * other.denominator_;
+		other.numerator_ = other.numerator_ * denominator_;
+	}
 	bool operator == (Fraction other)
 	{
-		return (numerator_ / denominator_) == (other.numerator_ / other.denominator_);
+		get_default_denominator(other);
+		return (abs(reserve_numerator - other.numerator_) < delta);
 	}
 	bool operator != (Fraction other)
 	{
@@ -22,7 +32,8 @@ public:
 	}
 	bool operator < (Fraction other)
 	{
-		return (numerator_ / denominator_) < (other.numerator_ / other.denominator_);
+		get_default_denominator(other);
+		return (reserve_numerator < other.numerator_);
 	}
 	bool operator > (Fraction other)
 	{
@@ -42,7 +53,6 @@ int main()
 {
 	Fraction f1(4, 3);
 	Fraction f2(6, 11);
-
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
